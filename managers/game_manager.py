9403352a3dd2af9373599bee93aa5820d51bf86b
Vika <119299120+vikasapfir@users.noreply.github.com>
2023-05-23 22:49:52 +0300

    fix lab with pylint

--- managers/game_manager.py
+++ managers/game_manager.py
@@ -1,36 +1,59 @@
+"""
+This module contains the GameManager class for managing games.
+"""
 
 class GameManager:
+    """
+    A class for managing games.
+    """
+    games = []
 
+    def add_game(self, game):
+        """Add a game to the list of games."""
+        self.games.append(game)
 
-  games = []
+    def remove_game(self, game):
+        """Remove a game from the list of games."""
+        self.games.remove(game)
 
+    def remove_by_index(self, index):
+        """Remove a game at the specified index from the list of games."""
+        self.games.pop(index)
 
-  
-  def add_game(self,game):
-    self.games.append(game)
+    def find_games_with_release_year_greater_than(self, release_year):
+        """
+        Find games with a release year greater than the specified year.
 
-  def remove_game(self,game):
-    self.games.remove(game)
+        Args:
+            release_year (int): The release year to compare.
 
-  def remove_by_index(self,index):
-    self.games.pop(index)
+        Returns:
+            list: A list of games with a release year greater than `release_year`.
+        """
+        return [game for game in self.games if game.release_year > release_year]
 
-  def find_games_with_release_year_greater_than(self, release_year):
+    def find_games_with_current_players_greater_than(self, current_players):
+        """
+        Find games with the current number of players greater than the specified number.
 
-    return [game for game in self.games if game.release_year > release_year]
+        Args:
+            current_players (int): The number of players to compare.
 
+        Returns:
+            list: A list of games with a current number of players greater than `current_players`.
+        """
+        return [game for game in self.games if game.current_players > current_players]
 
-  def find_games_with_current_players_greater_than(self, current_players):
-    return [game for game in self.games if game.current_players > current_players]
+    def get_games(self):
+        """
+        Get the list of games.
 
-  def get_games(self):
-    return self.games
-  
-  def print_all_games(self):
-    for game in self.games:
-      print(game)
+        Returns:
+            list: The list of games.
+        """
+        return self.games
 
-
-
-  
-  
+    def print_all_games(self):
+        """Print all games in the list."""
+        for game in self.games:
+            print(game)
--- models/sports_game.py
+++ models/sports_game.py
@@ -1,29 +1,21 @@
-from models.game import Game
 """
 This module defines the SportsGame class and provides related functionality.
 """
-
+from models.game import Game
 
 class SportsGame(Game):
-  """
+    """
     Represents a sports game with title, current players, player limits, release_year, publisher.
     """
 
-  def __init__(self,
-               title="",
-               current_players=0,
-               min_players=0,
-               max_players=0,
-               release_year=0,
-               publisher="",
-               has_referee=False):
-    super().__init__(title, current_players, min_players, max_players,
-                     release_year, publisher)
+  def __init__(self, title="", current_players=0, min_players=0, max_players=0, \
+release_year=0, publisher="", has_referee=False):
+    super().__init__(title, current_players, min_players, max_players, release_year, publisher)
     self.has_referee = has_referee
 
   @staticmethod
   def get_instance():
-    """
+        """
         Returns the instance of the default Sports game.
         If the instance doesn't exist, creates a new one and returns it.
         """
@@ -32,21 +24,21 @@ class SportsGame(Game):
     return SportsGame.default_game
 
   def connect_player(self):
-    """
+        """
         Adds a player to the  game if the maximum number of players hasn't been reached.
         """
     if self.current_players < self.max_players:
       self.current_players += 1
 
   def disconnect_player(self):
-    """
+        """
         Removes a player from the game if there is at least one player.
         """
     if self.current_players > 0:
       self.current_players -= 1
 
   def __str__(self):
-    """
+      """
       Returns a string representation of the sports game.
-        """
+      """
     return super().__str__() + f",Has Referee: {self.has_referee}"
