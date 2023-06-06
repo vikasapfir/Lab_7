8cbe329726c0862c900bfeb71968d43cdddd2610
Vika <119299120+vikasapfir@users.noreply.github.com>
2023-06-06 03:30:28 +0300

    lab with pylint

--- models/boarding_game.py
+++ models/boarding_game.py
@@ -1,37 +1,39 @@
-from models.game import Game
 """
 This module defines the BoardingGame class and provides related functionality.
 """
-
+from models.game import Game
 
 class BoardingGame(Game):
-  """
-    Represents a boaring game with title, current players, player limits, release_year, publisher.
+    """
+    Represents a boarding game with title, current players, player limits, release_year, publisher.
     """
 
-  def __init__(self,
-               title="",
-               current_players=0,
-               min_players=0,
-               max_players=0,
-               release_year=0,
-               publisher="",
-              game_themes=[]):
-    super().__init__(title, current_players, min_players, max_players,
-                   release_year, publisher,game_themes)
-
-  @staticmethod
-  def get_instance():
-    """
+    @staticmethod
+    def get_instance():
+        """
         Returns the instance of the default board game.
         If the instance doesn't exist, creates a new one and returns it.
         """
-    if not BoardingGame.default_game:
-      BoardingGame.default_game = BoardingGame()
-    return BoardingGame.default_game
+        if not BoardingGame.default_game:
+            BoardingGame.default_game = BoardingGame()
+        return BoardingGame.default_game
 
-  def __str__(self):
-    """
-      Returns a string representation of the boarding game.
+    def connect_player(self):
         """
-    return super().__str__()
+        Adds a player to the game if the maximum number of players hasn't been reached.
+        """
+        if self.current_players < self.max_players:
+            self.current_players += 1
+
+    def disconnect_player(self):
+        """
+        Removes a player from the game if there is at least one player.
+        """
+        if self.current_players > 0:
+            self.current_players -= 1
+
+    def __str__(self):
+        """
+        Returns a string representation of the boarding game.
+        """
+        return super().__str__() + "test"
--- models/game.py
+++ models/game.py
@@ -1,74 +1,47 @@
-from abc import ABC, abstractmethod
-from exceptions import TooMuchPlayersException
-from exceptions import NegativeNumberOfPlayersException
-from decorator import logged
-
 """
 This module defines the Game class and provides related functionality.
 """
 
+from abc import ABC, abstractmethod
 
 class Game(ABC):
-  """
+    """
     Represents a board game with title, current players, player limits, release_year, publisher.
     """
-  default_game = None
 
-  def __init__(self,
-               title="",
-               current_players=0,
-               min_players=0,
-               max_players=0,
-               release_year=0,
-               publisher="",
-               game_themes=[]):
-    self.title = title
-    self.current_players = current_players
-    self.min_players = min_players
-    self.max_players = max_players
-    self.release_year = release_year
-    self.publisher = publisher
-    self.game_themes = game_themes
+    default_game = None
 
-  def __iter__(self):
-    return iter(self.game_themes)
+    def __init__(self, title="", current_players=0, min_players=0, max_players=0, \
+release_year=0, publisher=""):
+        self.title = title
+        self.current_players = current_players
+        self.min_players = min_players
+        self.max_players = max_players
+        self.release_year = release_year
+        self.publisher = publisher
 
-  def __getitem__(self, index):
-    return list(self.game_themes)[index]
-
-  @logged(TooMuchPlayersException,"file")
-  def connect_player(self):
-    """
-        Adds a player to the  game if the maximum number of players hasn't been reached.
+    @abstractmethod
+    def connect_player(self):
         """
-    if self.current_players < self.max_players:
-      self.current_players += 1
-    else:
-      raise TooMuchPlayersException()
-
-  @logged(NegativeNumberOfPlayersException,"file")
-  def disconnect_player(self):
-    """
-        Removes a player from the game if there is at least one player.
+        Abstract method to connect a player to the game.
         """
-    if self.current_players > 0:
-      self.current_players -= 1
-    else:
-      raise  NegativeNumberOfPlayersException()
 
-
-  def can_play(self):
-    """
+    @abstractmethod
+    def disconnect_player(self):
+        """
+        Abstract method to disconnect a player from the game.
+        """
+    def can_play(self):
+        """
         Checks if the current number of players is within the allowed range.
         Returns True if the game can be played, False otherwise.
         """
-    return self.min_players <= self.current_players <= self.max_players
+        return self.min_players <= self.current_players <= self.max_players
 
-  def __len__(self):
-    return len(self.game_themes)
-
-  def __str__(self):
-    """
-      Returns a string representation of the board game.
+    def __str__(self):
         """
-    return f"Title: {self.title}, Current Players: {self.current_players}, Min Players: {self.min_players}, Max Players: {self.max_players}, Release Year: {self.release_year},Publisher: {self.publisher}"
+        Returns a string representation of the board game.
+        """
+        return f"Title: {self.title}, Current Players: {self.current_players}, \
+Min Players: {self.min_players}, Max Players: {self.max_players}, \
+Release Year: {self.release_year}, Publisher: {self.publisher}"
